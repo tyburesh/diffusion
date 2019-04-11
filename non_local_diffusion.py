265c62bb1eb40a4a693e660e9fa97f531eca42fb
Tyler Buresh <TylerBuresh@x-10-0-0-72.crk.umn.edu>
2019-04-11 17:27:23 -0500

    Diffusion now uses distance decay

--- non_local_diffusion.py
+++ non_local_diffusion.py
@@ -13,8 +13,8 @@ from pycuda.compiler import SourceModule
 # constants
 MATRIX_SIZE = 8 # size of square grid
 BLOCK_SIZE = 2 # block dimensions
-PROBABILITY = 0.9 # probability of diffusion
-N_ITERS = 2 # number of iterations
+PROBABILITY = 0.75 # probability of diffusion
+N_ITERS = 5 # number of iterations
 
 class Diffusion:
 	def __init__(self, matrix, block, probability):
@@ -33,22 +33,66 @@ class Diffusion:
 	def initialize_kernel(self):
 		self.kernel_code = """
 
+			#include <stdlib.h>
+			#include <math.h>
+
 			// Ignore edge rows and columns
-			// Assuming the matrix is large, the effect of this is small
 			__global__ void diffuse(float* grid, float* new_grid, float* randoms, int* x_coords, int* y_coords)
 			{{
 
 				unsigned int grid_size = {};
 				float prob = {};
 
-				unsigned int x = threadIdx.x + blockIdx.x * blockDim.x;			// column element of index
-				unsigned int y = threadIdx.y + blockIdx.y * blockDim.y;			// row element of index
-				unsigned int thread_id = y * grid_size + x; 					// thread index in array
+				unsigned int x = threadIdx.x + blockIdx.x * blockDim.x;				// column element of index
+				unsigned int y = threadIdx.y + blockIdx.y * blockDim.y;				// row element of index
+				unsigned int thread_id = y * grid_size + x; 						// thread index in array
 
 				if (grid[thread_id] == 1) {{
-					new_grid[thread_id] = 1;									// current cell
+					new_grid[thread_id] = 1;										// current cell
 					if (randoms[thread_id] < prob) {{
-						unsigned int spread_index = y_coords[thread_id] * grid_size + x_coords[thread_id];
+
+						// row and col before distance decay
+						unsigned int random_x = x_coords[thread_id];
+						unsigned int random_y = y_coords[thread_id];
+
+						float diff = prob - randoms[thread_id];
+
+						// distance decay occuring in x and y directions
+						// amount of decay dictated by random coordinate, diffusion threshold, and random value
+						float decay_x = floor(abs(((float)random_x - x) / prob * diff));
+						float decay_y = floor(abs(((float)random_y - y) / prob * diff));
+
+						// apply decay in appropriate direction
+						unsigned int spread_x = random_x;
+						if (random_x > x) {{
+							spread_x -= decay_x;
+						}}
+						else if (random_x < x) {{
+							spread_x += decay_x;
+						}}
+
+						// apply decay in appropriate direction
+						unsigned int spread_y = random_y;
+						if (random_y > y) {{
+							spread_y -= decay_y;
+						}}
+						else if (random_y < y) {{
+							spread_y += decay_y;
+						}}
+
+						/*
+						printf("Initial y: %u\\t"
+							"Inintial x: %u\\t"
+							"Random y: %u\\t"
+							"Random x: %u\\t"
+							"Y decay: %f\\t"
+							"Decay x: %f\\t"
+							"New y: %u\\t"
+							"New x: %u\\n",
+							y, x, random_y, random_x, decay_y, decay_x, spread_y, spread_x);
+						*/
+
+						unsigned int spread_index = spread_y * grid_size + spread_x;
 						new_grid[spread_index] = 1;
 					}}
 				}}
@@ -73,7 +117,7 @@ class Diffusion:
 		self.random_y_coordinates = ((curandom.rand((self.size, self.size))) * self.size).astype(np.int32)
 
 	def run(self):
-		print('Starting grid: ', self.grid_gpu)
+		print('Starting grid: \n', self.grid_gpu)
 		i = 0
 		while i < N_ITERS:
 			self.diffusion(
@@ -97,7 +141,8 @@ class Diffusion:
 			self.random_x_coordinates = ((curandom.rand((self.size, self.size))) * self.size).astype(np.int32)
 			self.random_y_coordinates = ((curandom.rand((self.size, self.size))) * self.size).astype(np.int32)
 			i += 1
-		print('Final grid: ', self.grid_gpu)
+			print('\nGrid after iteration {}: \n{}'.format(i, self.grid_gpu))
+		print('\nFinal grid: \n', self.grid_gpu)
 
 if __name__ == '__main__':
 	Diffusion(MATRIX_SIZE, BLOCK_SIZE, PROBABILITY)
