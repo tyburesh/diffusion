93c61226d5fefdc6670fca0b982cece0822b6ced
Tyler Buresh <TylerBuresh@x-10-0-0-72.crk.umn.edu>
2019-04-26 13:17:29 -0500

    Beginning of moving window

--- diffusion_modular.py
+++ diffusion_modular.py
@@ -14,7 +14,8 @@ from pycuda.compiler import SourceModule
 
 # constants
 MATRIX_SIZE = 8 # size of square grid
-BLOCK_SIZE = 2 # block dimensions
+BLOCK_DIMS = 2 # block dimensions
+GRID_DIMS = (MATRIX_SIZE + BLOCK_DIMS - 1) // BLOCK_DIMS # grid dimensions
 P_LOCAL = 1.0 # probability of local diffusion
 P_NON_LOCAL = 0.25 # probability of non-local diffusion
 N_ITERS = 1 # number of iterations
@@ -23,14 +24,15 @@ STREAM_SIZE = MATRIX_SIZE * MATRIX_SIZE // N_STREAMS # data elements per stream
 
 
 class Diffusion:
-	def __init__(self, matrix, block, local, non_local):
+	def __init__(self, matrix, block, grid, local, non_local):
 		self.size = matrix
-		self.n_blocks = matrix // block
-		self.n_threads = block
+		self.block_dims = block
+		self.grid_dims = grid
 		self.p_local = local
 		self.p_non_local = non_local
 		self.initialize_grid()
 		self.initialize_gpu_memory()
+		self.initialize_streams()
 		self.initialize_kernel()
 		self.run()	# unnecessary for FOREST implementation
 
@@ -41,10 +43,8 @@ class Diffusion:
 
 	# Allocate memory on GPU
 	def initialize_gpu_memory(self):
-		self.grid_a = gpuarray.empty(STREAM_SIZE, np.float32)
-		self.grid_b = gpuarray.empty(STREAM_SIZE, np.float32)
-	#	self.grid_a = gpuarray.to_gpu(self.grid)
-	#	self.grid_b = gpuarray.empty((self.size, self.size), np.float32)
+		self.gpu_grid_a = drv.mem_alloc(2500000000)
+		self.gpu_grid_b = drv.mem_alloc(2500000000)
 
 	# Create kernel and kernel functions
 	def initialize_kernel(self):
@@ -57,37 +57,42 @@ class Diffusion:
 
 				unsigned int x = threadIdx.x + blockIdx.x * blockDim.x;			// column element of index
 				unsigned int y = threadIdx.y + blockIdx.y * blockDim.y;			// row element of index
-				unsigned int thread_id = y * grid_size + x; 					// thread index in array
 
-				// edges will be ignored as starting points
-				unsigned int edge = (x == 0) || (x == grid_size - 1) || (y == 0) || (y == grid_size - 1);
+				if (x < grid_size && y < grid_size) {{
 
-				if (grid_a[thread_id] == 1) {{
-					grid_b[thread_id] = 1;										// current cell
-					if (!edge) {{
-						if (randoms[thread_id - grid_size] < prob) {{
-							grid_b[thread_id - grid_size] = 1;					// above
-						}}
-						if (randoms[thread_id - grid_size - 1] < prob) {{
-							grid_b[thread_id - grid_size - 1] = 1;				// above and left
-						}}
-						if (randoms[thread_id - grid_size + 1] < prob) {{
-							grid_b[thread_id - grid_size + 1] = 1;				// above and right
-						}}
-						if (randoms[thread_id + grid_size] < prob) {{
-							grid_b[thread_id + grid_size] = 1;					// below
-						}}
-						if (randoms[thread_id + grid_size - 1] < prob) {{
-							grid_b[thread_id + grid_size - 1] = 1;				// below and left
-						}}
-						if (randoms[thread_id + grid_size + 1] < prob) {{
-							grid_b[thread_id + grid_size + 1] = 1;				// below and right
-						}}
-						if (randoms[thread_id - 1] < prob) {{
-							grid_b[thread_id - 1] = 1;							// left
-						}}
-						if (randoms[thread_id + 1] < prob) {{
-							grid_b[thread_id + 1] = 1;							// right
+					unsigned int thread_id = y * grid_size + x; 					// thread index in array
+					printf("Thread ID = %u\\n", thread_id);
+
+					// edges will be ignored as starting points
+					unsigned int edge = (x == 0) || (x == grid_size - 1) || (y == 0) || (y == grid_size - 1);
+
+					if (grid_a[thread_id] == 1) {{
+						grid_b[thread_id] = 1;										// current cell
+						if (!edge) {{
+							if (randoms[thread_id - grid_size] < prob) {{
+								grid_b[thread_id - grid_size] = 1;					// above
+							}}
+							if (randoms[thread_id - grid_size - 1] < prob) {{
+								grid_b[thread_id - grid_size - 1] = 1;				// above and left
+							}}
+							if (randoms[thread_id - grid_size + 1] < prob) {{
+								grid_b[thread_id - grid_size + 1] = 1;				// above and right
+							}}
+							if (randoms[thread_id + grid_size] < prob) {{
+								grid_b[thread_id + grid_size] = 1;					// below
+							}}
+							if (randoms[thread_id + grid_size - 1] < prob) {{
+								grid_b[thread_id + grid_size - 1] = 1;				// below and left
+							}}
+							if (randoms[thread_id + grid_size + 1] < prob) {{
+								grid_b[thread_id + grid_size + 1] = 1;				// below and right
+							}}
+							if (randoms[thread_id - 1] < prob) {{
+								grid_b[thread_id - 1] = 1;							// left
+							}}
+							if (randoms[thread_id + 1] < prob) {{
+								grid_b[thread_id + 1] = 1;							// right
+							}}
 						}}
 					}}
 				}}
@@ -101,13 +106,17 @@ class Diffusion:
 
 				unsigned int x = threadIdx.x + blockIdx.x * blockDim.x;			// column element of index
 				unsigned int y = threadIdx.y + blockIdx.y * blockDim.y;			// row element of index
-				unsigned int thread_id = y * grid_size + x; 					// thread index in array
 
-				if (grid_a[thread_id] == 1) {{
-					grid_b[thread_id] = 1;									// current cell
-					if (randoms[thread_id] < prob) {{
-						unsigned int spread_index = y_coords[thread_id] * grid_size + x_coords[thread_id];
-						grid_b[spread_index] = 1;
+				if (x < grid_size && y < grid_size) {{
+
+					unsigned int thread_id = y * grid_size + x; 					// thread index in array
+
+					if (grid_a[thread_id] == 1) {{
+						grid_b[thread_id] = 1;									// current cell
+						if (randoms[thread_id] < prob) {{
+							unsigned int spread_index = y_coords[thread_id] * grid_size + x_coords[thread_id];
+							grid_b[spread_index] = 1;
+						}}
 					}}
 				}}
 			}}
@@ -117,6 +126,19 @@ class Diffusion:
 		self.mod = SourceModule(self.kernel)
 		self.local_diffusion = self.mod.get_function('local_diffuse')
 		self.non_local_diffusion = self.mod.get_function('non_local_diffuse')
+
+	def initialize_streams(self):
+		self.streams = []
+		self.events = []
+		self.reference = drv.Event()
+		for k in range(N_STREAMS):
+			self.streams.append(drv.Stream())
+			self.events.append({
+				'local_kernel_begin':drv.Event(),
+				'local_kernel_end':drv.Event(),
+				'non_local_kernel_begin':drv.Event(),
+				'non_local_kernel_end':drv.Event()
+			})
 
 	# Gererate random values between [0,1)
 	# Diffusion will occur / not occur based on these values
@@ -132,47 +154,79 @@ class Diffusion:
 	# Performs one iteration of local diffusion
 	def local(self):
 		print('\nGrid before local diffusion: \n', self.grid)
+
 		for i in range(N_STREAMS):
 
-			offset = i * STREAM_SIZE
+			start_index = i * (STREAM_SIZE // MATRIX_SIZE)
+			end_index = start_index + (STREAM_SIZE // MATRIX_SIZE)
 
-			# TODO: fix indexing of self.grid
-			self.grid_a = gpuarray.to_gpu(self.grid[0:2])
-			self.grid_b = gpuarray.to_gpu(self.grid[0:2])
+			# Need to figure out indexing of self.grid
+			# Pass in quadrants rather than rows
+			# Generate random numbers to be equal to the size of the quadrant
+			if i == 0: # top left
+				drv.memcpy_htod(self.gpu_grid_a, np.array([self.grid[i][:2] for i in range(2)]))
+			elif i == 1: # top right
+				drv.memcpy_htod(self.gpu_grid_a, np.array([self.grid[i][2:] for i in range(2)]))
+			elif i == 2: # bottom left
+				drv.memcpy_htod(self.gpu_grid_a, np.array([self.grid[i][:2] for i in range(2,4)]))
+			elif i == 3: # bottom right
+				drv.memcpy_htod(self.gpu_grid_a, np.array([self.grid[i][2:] for i in range(2,4)]))
 
+			self.events[i]['local_kernel_begin'].record(self.streams[i])
 			self.local_diffusion(
-				self.grid_a, self.grid_b, self.randoms,
-				grid = (self.n_blocks, self.n_blocks, 1),
-				block = (self.n_threads, self.n_threads, 1),)
-			self.grid_a, self.grid_b = self.grid_b, self.grid_a
+				self.gpu_grid_a, self.gpu_grid_b, self.randoms,
+				grid = (self.grid_dims, self.grid_dims, 1),
+				block = (self.block_dims, self.block_dims, 1),
+				stream = self.streams[i],)
+			self.events[i]['local_kernel_end'].record(self.streams[i])
+			self.gpu_grid_a, self.gpu_grid_b = self.gpu_grid_b, self.gpu_grid_a
 
-			# TODO: fix indexing of self.grid
-			self.grid[0:2] = self.grid_a.get()
+			drv.memcpy_dtoh(self.grid[start_index:end_index], self.gpu_grid_a)
 
 		print('\nGrid after local diffusion: \n', self.grid)
 
 	# Performs one iteration of non_local diffusion
 	def non_local(self):
-		print('\nGrid before non_local diffusion: \n', self.grid_b)
-		self.non_local_diffusion(
-			self.grid_b, self.grid_a, self.randoms, 
-			self.random_x_coordinates, self.random_y_coordinates,
-			grid = (self.n_blocks, self.n_blocks, 1),
-			block = (self.n_threads, self.n_threads, 1),)
-		self.grid_a, self.grid_b = self.grid_b, self.grid_a
-		print('\nGrid after non_local diffusion: \n', self.grid_a)
+		print('\nGrid before non_local diffusion: \n', self.gpu_grid_b)
+
+		for i in range(N_STREAMS):
+
+			start_index = i * (STREAM_SIZE // MATRIX_SIZE)
+			end_index = start_index + (STREAM_SIZE // MATRIX_SIZE)
+
+			drv.memcpy_htod(self.gpu_grid_a, self.grid[start_index:end_index])
+
+			self.events[i]['non_local_kernel_begin'].record(self.streams[i])
+			self.non_local_diffusion(
+				self.gpu_grid_b, self.gpu_grid_a, self.randoms, 
+				self.random_x_coordinates, self.random_y_coordinates,
+				grid = (self.grid_dims, self.grid_dims, 1),
+				block = (self.block_dims, self.block_dims, 1),
+				stream = self.streams[i])
+			self.events[i]['non_local_kernel_end'].record(self.streams[i])
+			self.gpu_grid_a, self.gpu_grid_b = self.gpu_grid_b, self.gpu_grid_a
+
+			drv.memcpy_dtoh(self.grid[start_index:end_index], self.gpu_grid_a)
+
+		print('\nGrid after non_local diffusion: \n', self.gpu_grid_a)
 
 	# unnecessary for FOREST implementation
 	def run(self):
 		i = 0
+		self.reference.record()
 		while i < N_ITERS:
 			self.generate_randoms()
 			self.local()
 			#self.generate_random_coords()
 			#self.non_local()
 			i += 1
-		#print('Final Board: ', grid_b.get())
+		print('\n')
+		for k in range(N_STREAMS):
+			print('Local_kernel_begin: ', self.reference.time_till(self.events[k]['local_kernel_begin']))
+			print('Local_kernel_end: ', self.reference.time_till(self.events[k]['local_kernel_end']))
+			#print('Non_local_kernel_begin: ', self.reference.time_till(self.events[k]['non_local_kernel_begin']))
+			#print('Non_local_kernel_end: ', self.reference.time_till(self.events[k]['non_local_kernel_end']))
 
 if __name__ == '__main__':
-	Diffusion(MATRIX_SIZE, BLOCK_SIZE, P_LOCAL, P_NON_LOCAL)
+	Diffusion(MATRIX_SIZE, BLOCK_DIMS, GRID_DIMS, P_LOCAL, P_NON_LOCAL)
 
